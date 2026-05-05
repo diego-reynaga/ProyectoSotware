a84e4af7d9d2ea50090f4bff890db951b146f265
diego-reynaga <diegoreynaga666@gmail.com>
2026-05-04 21:44:04 -0500

    ahora la BD es relacional

--- database/db.sql
+++ database/db.sql
@@ -1,16 +1,6 @@
-CREATE TABLE usuarios(
-id_usuario INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nombres     VARCHAR(255) NOT NULL,
-cargo       VARCHAR(255) NOT NULL,
-email       VARCHAR(255) NOT NULL UNIQUE KEY,
-password    TEXT NOT NULL,
-fyh_creacion DATETIME NULL,
-fyh_actualizacion DATETIME NULL,
-estado      VARCHAR(11)
-)ENGINE=InnoDB;
+CREATE DATABASE IF NOT EXISTS gestionestudiante;
+USE gestionestudiante;
 
-INSERT INTO usuarios(nombres,cargo,email,password, fyh_creacion, estado) 
-VALUES ('John Doe', 'admin', 'admin@admin.com', 'hashed_password', NOW(), 'activo');
 
 
 CREATE TABLE roles(
@@ -25,3 +15,20 @@ estado      VARCHAR(11)
 insert into roles(nombres_rol, fyh_creacion, estado) values ('Administrador', NOW(), 'activo');
 insert into roles(nombres_rol, fyh_creacion, estado) values ('Director Academico', NOW(), 'activo');
 insert into roles(nombres_rol, fyh_creacion, estado) values ('Secretaria', NOW(), 'activo');
+
+CREATE TABLE usuarios(
+id_usuario INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nombres     VARCHAR(255) NOT NULL,
+rol_id       INT (11) NOT NULL,
+email       VARCHAR(255) NOT NULL UNIQUE KEY,
+password    TEXT NOT NULL,
+fyh_creacion DATETIME NULL,
+fyh_actualizacion DATETIME NULL,
+estado      VARCHAR(11),
+FOREIGN KEY (rol_id) REFERENCES roles(id_rol) ON DELETE NO ACTION ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+INSERT INTO usuarios(nombres,rol_id,email,password, fyh_creacion, estado) 
+VALUES ('John Doe', '1', 'admin@admin.com', 'hashed_password', NOW(), 'activo');
+
+
